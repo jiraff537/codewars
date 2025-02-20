b1a462e06fe5bf8d89ca0c7628d84b09ac60b3d2
jiraff537 <alexei.danilov@gmail.com>
2025-02-20 01:22:44 +0100

    add filter from specsymbols and bad incoming data

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,6 @@
 package com.company;
 
-public class Main {
+public class Main { //https://www.codewars.com/kata/51b66044bce5799a7f000003/train/java
 
     public static void main(String[] args) {
 	// write your code here
